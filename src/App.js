fe45d5fb43abb7e1cfeedd75f6d0beddff887a0e
azar <azarniftali@ukr.net>
2022-09-28 12:54:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import '../src/App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [counter, setCounter] = React.useState(0);
+    
+  const incCounter = () => {
+    if (counter < + 50) {
+        setCounter(counter + 1);
+      }
+    } 
+     
+  const decCounter = () => {
+    if (counter > -50) {
+        setCounter(counter - 1);
+      }
+    } 
+    
+    const rndCounter = () => {
+      setCounter (+(Math.random() * (50 - -50) + -50).toFixed(0))
+    }
+    
+     const resetCounter = () => {
+        setCounter(0)
+       } 
 
-export default App;
+      return (
+        <div class="app">
+          <div class="counter">{counter}</div>
+          <div class="controls">
+            <button onClick={incCounter}>INC</button>
+            <button onClick={decCounter}>DEC</button>
+            <button onClick={rndCounter}>RND</button>
+            <button  onClick={resetCounter}>RESET</button>
+          </div>
+        </div>
+      )
+    }
+  export default App;
+ 
